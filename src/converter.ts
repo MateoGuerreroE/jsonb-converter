0cfa0541fb0436664c4812c5d5c60bd805417c8c
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-04-13 09:22:18 -0500

    Added support for diff types

--- src/converter.ts
+++ src/converter.ts
@@ -1,21 +1,23 @@
 export default class converter {
     private columnNames: string[] = [];
-    private rawElements: any[] = [];
+    private rawElements: Object[] = [];
     private tableName: string;
+    private typeList: string[];
 
     constructor(object: string, tableName: string) {
         this.tableName = tableName;
         this.rawElements = JSON.parse(object.replace(/'/g, '`'));
         this.columnNames = Object.keys(this.rawElements[0]);
+        this.verifyTypes();
     }
 
     public createQuery(): string {
-        return `INSERT INTO ${this.tableName} (${this.columnNames.join(', ')}) VALUES\n${this.assignValue()}`;
+        return `INSERT INTO ${this.tableName} (${this.columnNames.join(', ')}) VALUES\n${this.insertValues()}`;
     }
 
-    private assignValue(): string {
-        const stringElementValues: string[] = this.rawElements.map((element) => this.getStringValues(element));
-        return stringElementValues.map(element => ` (${element})`).join(',\n')
+    private insertValues(): string {
+        const ElementValues: string[] = this.rawElements.map((element) => this.createInsertStatement(element));
+        return ElementValues.map(element => ` (${element})`).join(',\n')
     }
 
     private getStringValues(object: any): string {
@@ -38,6 +40,46 @@ export default class converter {
             }
             else return 'null';
         }).join(', ')
+    }
+
+    private createInsertStatement(object: Object): string {
+        return Object.values(object).map((element, index) => {
+            let resultingElement: string;
+            switch (this.typeList[index]) {
+                case 'string':
+                    if (element.length === 0) resultingElement = `NULL`;
+                    resultingElement = `'${element.trim()}'`;
+                    break;
+                case 'object':
+                    if (Array.isArray(element)) {
+                        if (element.length > 1) {
+                            const arrayOfInsertions = element.map(item => `jsonb_insert('[]', array['0'], '"${item.trim()}"')`)
+                            resultingElement = arrayOfInsertions.join(' || ');
+                        } else {
+                            resultingElement = `jsonb_insert('[]', array['0'], '"${element[0]}"')`
+                        }
+                    } else throw new Error('JSON not supported yet') // TODO: Add JSON Support
+                    break;
+                case 'number':
+                case 'double':
+                    resultingElement = `${element}`
+                    break;
+                default: 
+                    resultingElement = `NULL`;
+            }
+            return resultingElement;
+        }).join(', ')
+    }
+
+    private verifyTypes() {
+        const validTypes = ['object', 'string', 'number', 'double'];
+        const typeList: string[] = [];
+        const sampleElement = this.rawElements[0];
+        for (const element of Object.values(sampleElement)) {
+            if (validTypes.includes(typeof element)) typeList.push(typeof element);
+            else throw new Error(`Unsupported type: ${typeof element}`)
+        }
+        this.typeList = typeList;
     }
 
 }
